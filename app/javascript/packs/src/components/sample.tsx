2b0d1ffcf65aba1f4e841d2c54cf3fc4e33ff9c3
“takuma” <fsww2353@gmail.com>
2020-01-02 16:52:23 +0900

    typescript env

---
+++ app/javascript/packs/src/components/sample.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Sample: React.FC = () => (
+  <div>messages block</div>
+);
+
+export default Sample
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.append('json', {
+  test: /\.(ts|tsx)?(\.erb)?$/,
+  use: [{
+    loader: 'ts-loader'
+  }]
+})
+
 module.exports = environment
